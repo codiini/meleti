c5e46e86c9190acdfa9eceef5e5401f45ecb0653
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-08-31 09:14:30 +0100

    chore: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <div class="flex relative">
+      <TheSideBar></TheSideBar>
+      <UserMenu class="absolute right-0"></UserMenu>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import UserMenu from "@/components/dashboard/UserMenu.vue";
+</script>
