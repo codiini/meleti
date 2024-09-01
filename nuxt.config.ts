34150c84df25c4a568530055a60558ec34815f1f
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-09-01 23:47:02 +0100

    chore: remove nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@nuxthub/core"],
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
   compatibilityDate: "2024-08-30",
-  hub: {
-    blob: true,
-    remote: true,
-  },
   supabase: {
     redirect: false,
     redirectOptions: {
