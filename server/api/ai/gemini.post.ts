c384a0c77730157e562c976c19bb93f0428e83ce
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-09-12 05:43:51 +0100

    update vertex auth

--- server/api/ai/gemini.post.ts
+++ server/api/ai/gemini.post.ts
@@ -2,12 +2,19 @@ import { VertexAI } from "@google-cloud/vertexai";
 import { MULTIPLE_CHOICE_PROMPT } from "~/utils/prompts";
 
 export default defineEventHandler(async (event) => {
+  const GOOGLE_APPLICATION_CREDENTIALS = JSON.parse(
+    process.env.GOOGLE_APPLICATION_CREDENTIALS as string
+  );
+
   const body = await readBody(event);
   const { fileContent, maxQuestions, difficulty } = body;
 
   const vertex_ai = new VertexAI({
     project: process.env.VERTEX_PROJECT,
     location: process.env.VERTEX_REGION,
+    googleAuthOptions: {
+      credentials: GOOGLE_APPLICATION_CREDENTIALS,
+    },
   });
 
   const model = "gemini-1.5-flash-001";
