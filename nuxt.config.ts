76f895c6c390835e6e3333f2216e759c9e3c3866
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-09-12 01:56:17 +0100

    feat: setup vercel deployment and google vertex auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxtjs/supabase"],
-  // nitro: {
-  //   preset: "cloudflare-pages",
-  // },
+  nitro: {
+    preset: "vercel",
+  },
   compatibilityDate: "2024-08-30",
   supabase: {
     redirect: false,
